0cfd52cdf8e4c3b3f92ce930947324a7ba012c1e
kaki1013 <lmklove0@naver.com>
2022-01-01 17:43:28 +0900

    Update 2294.py
    2293번 풀이 응용하여 아예 다시 풀이

--- 2294.py
+++ 2294.py
@@ -1,23 +1,12 @@
-# dp[n][k] := 동전을 0번부터 n-1번까지 사용하여 k원을 만드는 최소 동전 개수
-# dp[0][x] = 0 으로 정의
-# dp[n][k] := min(dp[n-1][k-coins[n]], dp[n][k-koins[n]]) + 1
-# 10**6 초기화 후 -1로 치환
 n, k = map(int, input().split())
 coins = [int(input()) for _ in range(n)]
 
-dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
-for i in range(1, n+1):
-    coin = coins[i-1]
-    for j in range(k+1):
-        if j - coin == 0:
-            dp[i][j] = 1
-        elif j - coin > 0:
-            if dp[i-1][j-coin] != 0 or dp[i][j-coin] != 0:
-                dp[i][j] = min({dp[i-1][j-coin], dp[i][j-coin]} - {0}) + 1
-        if dp[i-1][j] != 0:
-            if dp[i][j] == 0:
-                dp[i][j] = dp[i-1][j]
-            else:
-                dp[i][j] = min(dp[i-1][j], dp[i][j])
-
-print(dp[n][k] if dp[n][k] else -1)
+# dp[i;k] := i원을 만드는 데 필요한 동전의 최소 개수(반복문 j번 -> coins 0번부터 j-1번까지 사용) / 0원: 0개
+dp = [10**5 for i in range(k+1)]    # 초기화
+dp[0] = 0
+for i in range(n):                              # 동전 하나씩 사용
+    for j in range(1, k+1):                     # 금액 하나씩 갱신
+        now = coins[i]                          # 현재 사용 중인 금액
+        if j >= now:                            # 현재 동전이 사용할 수 있다면
+            dp[j] = min(dp[j], dp[j-now] + 1)   # 현재 동전을 사용함
+print(-1 if dp[k] == 10**5 else dp[k])
