297c50aa7f511dbdfc868d0ff8fa3df90c81c410
Myung-Kyu Lee <65400693+kaki1013@users.noreply.github.com>
2022-07-12 20:04:21 +0900

    Add files via upload

--- c++_study/2738.cpp
+++ c++_study/2738.cpp
@@ -1,1 +1,40 @@
-// 참고 : https://leeeegun.tistory.com/3
+/*
+https://blog.naver.com/isaac7263/221633998534
+c++에서 c언어의 표준함수 호출하기
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, M, x;
+    vector<vector<int>> matrix;
+    vector<int> v;
+    
+    scanf("%d %d", &N, &M);
+
+    for (int i=0; i<N; i++) {
+        v.clear();
+        for (int j=0; j<M; j++) {
+            scanf("%d", &x);
+            v.push_back(x);
+        }
+        matrix.push_back(v);
+    }
+
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++) {
+            scanf("%d", &x);
+            matrix[i][j] += x;
+        }
+    }
+    
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<M; j++)
+            printf("%d ", matrix[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
