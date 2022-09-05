d513d43087ef65b7ea9c962553dbf598414a57f8
Myung-Kyu Lee <65400693+kaki1013@users.noreply.github.com>
2022-09-05 12:28:50 +0900

    Update 4355.py

--- 4355.py
+++ 4355.py
@@ -1,12 +1,27 @@
 import sys
+input = sys.stdin.readline
+
+def coprime(n):
+    ans = n
+
+    p = []
+    i = 2
+    while i <= int(n**0.5)+1:
+        if n % i == 0:
+            p.append(i)
+            while n % i == 0:
+                n //= i
+        i += 1
+    if n != 1:
+        p.append(n)
+    
+    
+    for pp in p:
+        ans = ans // pp * (pp-1)
+    return ans
+
 while True:
-    n = int(sys.stdin.readline().rstrip())
-    if n == 0:
+    n = int(input())
+    if not n:
         break
-    s = set(range(1, n))
-    if n in range(1, 6):
-        print([0, 0, 1, 2, 2, 4][n])
-    else:
-        for i in range(2, n+1):
-            if n % i == 0:
-                pass
+    print(coprime(n))
